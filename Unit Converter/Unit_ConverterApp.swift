e132f2a93f7ba91899d23b5b19e3482a9cf7bc62
Shlok Sinha <shlokjp@gmail.com>
2023-11-14 15:13:02 +0530

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Shlok Sinha on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
